eada765be43d6e7f269982e10f42a60312154756
Ruterryter <hanna.ruter@gmail.com>
2020-09-21 11:32:43 +0200

    adding favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Get your own cool Roller Derby Name here!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
