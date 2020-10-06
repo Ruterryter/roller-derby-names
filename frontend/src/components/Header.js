219094808d1d6d92e8556133328ea9ae7fee1e85
Ruterryter <hanna.ruter@gmail.com>
2020-10-06 12:55:32 +0200

    changes in mediaquery header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,10 +21,10 @@ const Heading = styled.h1`
 
   &:hover {
     text-decoration: none;
-    color: #86007D;
+    color: #86007D; }
 
-  @media (max-width: 1200px) {
-  font-size: 18px;
+  @media (max-width: 768px) {
+    width: 350px;}
 }
 `
 
